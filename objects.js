519973c23d6ff8ae2dd6841fb58c6d2d61a7ca8a
NeerajKumar22 <ineerajkr@gmail.com>
2020-01-06 23:25:22 +0530

    added topic about objects basically everything in javascript is object

---
+++ objects.js
@@ -0,0 +1,13 @@
+// Objects are everywhere
+
+// JavaScript Data types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+// 8. Objects
+
+// First 7 data types are primitives, these are immutable means they cannot be change
